fd43044225c2832931db9185e1675bfd1615369f
ppasta188-eng <ppasta188@gmail.com>
2026-04-25 04:05:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,16 @@
 import express from "express";
-import cors from "cors";
-import { scanGames } from "./routes/scan.js";
+import scanRoutes from "./routes/scan.js";
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
-// Teste
+app.use("/scan", scanRoutes);
+
 app.get("/", (req, res) => {
   res.send("API de apostas rodando 🚀");
 });
 
-// Scanner (AGORA COM DADOS REAIS)
-app.get("/scan", async (req, res) => {
-  try {
-    const dados = await scanGames();
-    res.json(dados);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ erro: "Erro ao buscar jogos" });
-  }
-});
-
-// Porta obrigatória Render
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
