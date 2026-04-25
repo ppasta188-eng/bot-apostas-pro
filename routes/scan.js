2c60943ce0da676dd0c4c7325db526c3d916bc11
ppasta188-eng <ppasta188@gmail.com>
2026-04-25 02:33:15 -0300

    Update scan.js

--- routes/scan.js
+++ routes/scan.js
@@ -1,1 +1,18 @@
-// teste
+import { getFixtures, getOdds } from "../serviços/apiService.js";
+import { analyzeGame } from "../serviços/analysisService.js";
+
+export async function scanGames() {
+  const jogos = await getFixtures();
+
+  let resultados = [];
+
+  for (let jogo of jogos) {
+    const odds = await getOdds(jogo.fixture.id);
+
+    const analise = analyzeGame(jogo, odds);
+
+    resultados.push(analise);
+  }
+
+  return resultados;
+}
