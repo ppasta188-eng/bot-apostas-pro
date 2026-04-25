2afc3b8b9e45a9621c02d95b8e2e2f73f5eec554
ppasta188-eng <ppasta188@gmail.com>
2026-04-25 02:30:58 -0300

    Update apiService.js

--- services/apiService.js
+++ services/apiService.js
@@ -1,1 +1,23 @@
-// teste
+import { API_KEY, BASE_URL } from "../configuração/api.js";
+
+export async function getFixtures() {
+  const response = await fetch(`${BASE_URL}/fixtures?next=10`, {
+    headers: {
+      "x-apisports-key": API_KEY
+    }
+  });
+
+  const data = await response.json();
+  return data.response;
+}
+
+export async function getOdds(fixtureId) {
+  const response = await fetch(`${BASE_URL}/odds?fixture=${fixtureId}`, {
+    headers: {
+      "x-apisports-key": API_KEY
+    }
+  });
+
+  const data = await response.json();
+  return data.response;
+}
