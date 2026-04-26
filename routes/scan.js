40c21b98d51dd329e6d986ab7a83065479d3ff5c
ppasta188-eng <ppasta188@gmail.com>
2026-04-26 12:45:31 -0300

    Update scan.js

--- routes/scan.js
+++ routes/scan.js
@@ -1,16 +1,11 @@
 import express from "express";
-import { analisarJogos } from "../services/analysisService.js";
+import { scanGames } from "../services/analysisService.js";
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  try {
-    const dados = await analisarJogos();
-    res.json(dados);
-  } catch (error) {
-    console.error("ERRO NA ROTA:", error.message);
-    res.status(500).json({ erro: "Erro ao analisar jogos" });
-  }
+  const data = await scanGames();
+  res.json(data);
 });
 
 export default router;
