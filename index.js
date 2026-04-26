18877cb47a37565f8f15104aeae929a9036541f5
ppasta188-eng <ppasta188@gmail.com>
2026-04-26 12:33:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
   res.send("API de apostas rodando 🚀");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
